6f720d54ee249a8707dce4f4ea88b056e5851d32
Guilherme Queiroz <guihsq1408@gmail.com>
2021-03-25 22:02:56 -0300

    Reorganized folders structure

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static("public"))
+
 server.get('/', (request, response) => {
-  return response.send('Hello World')
+  return response.sendFile(__dirname + "/views/index.html")
 })
 
 server.listen(3000, () => console.log('Server started!'))
